73fa4ac8f9e50c80aa7f9e942b4287a5ce9bf69b
Fabelair <felixantoinebelair@gmail.com>
2025-06-06 18:37:37 -0400

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ import Liste from './components/liste';
 import Buy from './components/buy';
 function App() {
 
-
   return (
-    <div class="select-none h-[98vh] grid grid-rows-10">
-      <div class="place-self-center"><h1 class="text-4xl">TheMillionSeconds.com</h1></div>
-      <div class=" place-self-center"><Counter/></div>
-      <div class=" row-span-4 place-self-center mt-10"><Content/></div>
-      <div class=" place-self-center"><Buy/></div>
-      <div class=" row-span-3 place-self-center"><Liste/></div>
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/" element={
+              <div class="select-none h-[98vh] grid grid-rows-10">
+                <div class="place-self-center"><h1 class="text-4xl">TheMillionSeconds.com</h1></div>
+                <div class=" place-self-center"><Counter/></div>
+                <div class=" row-span-4 place-self-center mt-10"><Content/></div>
+                <div class=" place-self-center"><Buy/></div>
+                <div class=" row-span-3 place-self-center"><Liste/></div>
+              </div>
+            }></Route>
+      </Routes>
+    </Router>
+
   )
 }
 
